6a6e75391d2d69849e02bcc398a798c56df7a56a
Mario Garcia <mario.ggar@gmail.com>
2015-09-07 02:30:36 +0200

    Add change to use AST @OptionMenu

--- mgug-app/src/main/groovy/mgug/app/MainActivity.groovy
+++ mgug-app/src/main/groovy/mgug/app/MainActivity.groovy
@@ -1,16 +1,16 @@
 package mgug.app
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import com.arasthel.swissknife.annotations.OnUIThread
 import mgug.app.adapter.RepositoryAdapter
 import mgug.app.services.Services
 import mgug.app.widget.CustomListActivity
-import mgug.app.widget.Widgets
+import mgug.lib.asts.OnOptionsItemSelected
+import mgug.lib.asts.OptionsMenu
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
+@OptionsMenu(R.menu.menu_main)
 class MainActivity extends CustomListActivity<RepositoryAdapter> {
 
     @Override
@@ -21,29 +21,12 @@ class MainActivity extends CustomListActivity<RepositoryAdapter> {
         loadTopicList()
     }
 
-    @Override
-    boolean onCreateOptionsMenu(Menu menu) {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    @Override
-    boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.itemId
-
-        if (id == R.id.action_about) {
-            showAbout()
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
     @OnUIThread
+    @OnOptionsItemSelected(R.id.action_about)
     void showAbout() {
         showDialogWithLayout(R.layout.about){
             icon  = R.drawable.ic_launcher
-            title = R.string.app_name
+            title = getString(R.string.app_name)
         }
     }
 
